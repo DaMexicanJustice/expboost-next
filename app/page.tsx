3078327746644c1a2844c5d5231c6518bdd467f3
DaMexicanJustice <mrrichtofen@gmail.com>
2024-10-04 21:22:46 +0200

    added call to action button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Divider from "./components/divider";
 import MobileNavbar from "./components/mobile-navbar";
 import Navbar from "./components/navbar";
+import PollCta from "./components/poll-cta";
 import StickySocials from "./components/sticky-socials";
 import Hero from "./hero/page";
 import Latest from "./latest/page";
@@ -18,10 +19,12 @@ export default function Home() {
         <div className="hidden xl:block">
           <StickySocials />
         </div>
+        <div className="hidden xl:block">
+          <PollCta />
+        </div>
         <main id="sections" className="">
           <Hero />
           <Divider />
-
           <Latest />
         </main>
       </div>
